54e59c732fedac8e8270d4ab412ffb822c2e2c26
Parker Parham <parkerparham@gmail.com>
2022-07-31 14:02:05 -0400

    Added reference notes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books") //sends promise
+  return fetch("https://anapioficeandfire.com/api/books") //sends promise and returns fetch of API data
   .then((resp) => resp.json()) //turns data into JSON
   .then((arrayOfBooks) => renderBooks(arrayOfBooks))
   
@@ -10,7 +10,7 @@ function fetchBooks() {
 }
 
 function renderBooks(books) {
-  console.log(books[0])
+  // console.log(books[0])
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -22,3 +22,8 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// fetch notes 
+// so for a fetch function, we return a fetch of an API to get the data
+// use .then to go through the data after it is loaded and turn it into a json
+// then we pass it into a function that does what we need to do with the data
