24754e33a1e34cbfec38cbff5cb637d1f5ca192c
Parker Parham <parkerparham@gmail.com>
2022-07-31 14:00:07 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books") //sends promise
+  .then((resp) => resp.json()) //turns data into JSON
+  .then((arrayOfBooks) => renderBooks(arrayOfBooks))
+  
+  // return resp.json()
+  // .then((json) => books = json)
   // To pass the tests, don't forget to return your fetch!
   
 }
 
 function renderBooks(books) {
+  console.log(books[0])
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
